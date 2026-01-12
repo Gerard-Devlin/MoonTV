f0952b34bfa1d5449a4aa73638e0d2f3f041410b
GerardDevlin <alwenxuyouwen@gmail.com>
2026-01-12 20:41:04 +0800

    change play page animation

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1785,9 +1785,32 @@ function PlayPageClient() {
                 {isVideoLoading && (
                   <div className='absolute inset-0 bg-black/85 backdrop-blur-sm rounded-xl flex items-center justify-center z-[500] transition-all duration-300'>
                     <div className='text-center max-w-md mx-auto px-6'>
-                      <aside className='play-pixel-loader' aria-hidden='true'>
-                        <div className='play-pixel-loader__pixel'></div>
-                      </aside>
+                      <svg
+                        className='play-heart'
+                        viewBox='-5 -5 278 56'
+                        version='1.1'
+                        xmlns='http://www.w3.org/2000/svg'
+                        aria-hidden='true'
+                      >
+                        <filter id='blur'>
+                          <feGaussianBlur stdDeviation='1.6'></feGaussianBlur>
+                        </filter>
+                        <g transform='translate(29.1 -127.42)'>
+                          <path
+                            pathLength='1'
+                            d='M-28.73 167.2c26.43 9.21 68.46-9.46 85.45-12.03 18.45-2.78 32.82 4.86 28.75 9.83-3.82 4.66-25.77-21.18-14.81-31.5 9.54-8.98 17.64 10.64 16.42 17.06-1.51-6.2 2.95-26.6 14.74-22.11 11.7 4.46-4.33 49.03-15.44 44.08-6.97-3.1 15.44-16.26 26.1-16 23.03.56 55.6 27.51 126.63 3.36'
+                            id='line'
+                          ></path>
+                        </g>
+                        <g transform='translate(29.1 -127.42)'>
+                          <path
+                            pathLength='1'
+                            d='M-28.73 167.2c26.43 9.21 68.46-9.46 85.45-12.03 18.45-2.78 32.82 4.86 28.75 9.83-3.82 4.66-25.77-21.18-14.81-31.5 9.54-8.98 17.64 10.64 16.42 17.06-1.51-6.2 2.95-26.6 14.74-22.11 11.7 4.46-4.33 49.03-15.44 44.08-6.97-3.1 15.44-16.26 26.1-16 23.03.56 55.6 27.51 126.63 3.36'
+                            id='point'
+                            filter='url(#blur)'
+                          ></path>
+                        </g>
+                      </svg>
 
                       {/* 换源消息 */}
                       <div className='space-y-2'>
